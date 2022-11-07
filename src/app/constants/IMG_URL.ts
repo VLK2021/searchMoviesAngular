34d26399f92c37e04ca8f118bfcc5200aeb9c3c8
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-11-07 20:15:18 +0200

    change flex

--- src/app/constants/IMG_URL.ts
+++ src/app/constants/IMG_URL.ts
@@ -1,1 +1,1 @@
-export const IMG_URL = 'https://image.tmdb.org/t/p/w300';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w185';
--- src/app/modules/movies/movies-component/movie/movie.component.ts
+++ src/app/modules/movies/movies-component/movie/movie.component.ts
@@ -1,18 +1,36 @@
 import {Component, Input, OnInit} from '@angular/core';
+
 import {IMovie} from "../../../../interfaces";
+import {IMG_URL} from "../../../../constants";
+
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
+
 export class MovieComponent implements OnInit {
+  IMG_URL: string = IMG_URL;
+
+
   @Input()
   movie: IMovie
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
-  }
+  };
+
+  // setNewClass(vote: number) {
+  //   if (vote >= 8) {
+  //     return 'green'
+  //   } else if (vote >= 6) {
+  //     return 'orange'
+  //   } else {
+  //     return 'red'
+  //   }
+  // };
 
 }
