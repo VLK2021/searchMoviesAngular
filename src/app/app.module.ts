dc43b97a491474947c56f65f2083a97339a73e0e
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-11-16 23:50:59 +0200

    add pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './app-components/header/header.component';
 import { FooterComponent } from './app-components/footer/footer.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { NavigationComponent } from './app-components/navigation/navigation.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import {AppRoutingModule} from "./app-routing.module";
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
