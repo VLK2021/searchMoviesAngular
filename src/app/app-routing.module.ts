5e130df82528e560b15166947672a821978636ac
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-11-21 01:04:53 +0200

    deleted searchMovies module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {RouterModule, Routes} from "@angular/router";
 let routes:Routes = [
   {path: '', redirectTo: 'movies', pathMatch: "full"},
   {path:'movies', loadChildren:() => import('./modules/movies/movies.module').then(m=>m.MoviesModule)},
-  {path:'serials', loadChildren:() => import('./modules/serials/serials.module').then(m=>m.SerialsModule)}
+  {path:'serials', loadChildren:() => import('./modules/serials/serials.module').then(m=>m.SerialsModule)},
 ];
 
 @NgModule({
