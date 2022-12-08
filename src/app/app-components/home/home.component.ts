fde4bd525cdd75f81f1bcc436fa1bfe39b6808df
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-08 21:35:49 +0200

    home component

--- src/app/app-components/home/home.component.ts
+++ src/app/app-components/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {MoviesService} from "../../modules/movies/movie-services";
-import {IMovie, ISerial} from "../../interfaces";
-import { IMG_URL } from '../../constants';
+import {IActor, IMovie, ISerial} from "../../interfaces";
+import {IMG_URL} from '../../constants';
 import {SerialsService} from "../../modules/serials/serials-services";
+import {ActorService} from "../../modules/actors/actors-services/actor.service";
 
 
 @Component({
@@ -15,21 +16,28 @@ import {SerialsService} from "../../modules/serials/serials-services";
 export class HomeComponent implements OnInit {
   movies: IMovie[];
   serials: ISerial[];
-  page:number = 1;
+  actors: IActor[];
+  page: number = 1;
   IMG_URL: string = IMG_URL;
 
   constructor(private moviesService: MoviesService,
-              private serialsService: SerialsService) { }
+              private serialsService: SerialsService,
+              private actorService: ActorService) {
+  }
 
   ngOnInit(): void {
     //@ts-ignore
     this.moviesService.getAll(this.page).subscribe(({results}) => {
       this.movies = results;
-    })
+    });
     //@ts-ignore
     this.serialsService.getAll(this.page).subscribe(({results}) => {
       this.serials = results;
-    })
+    });
+    //@ts-ignore
+    this.actorService.getAll(this.page).subscribe(({results}) => {
+      this.actors = results
+    });
   }
 
 }
