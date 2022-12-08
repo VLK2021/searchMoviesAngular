e768608d9c8c02f344829a60ac7e26b6407e36b0
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-08 10:49:39 +0200

    home component

--- src/app/app-components/home/home.component.ts
+++ src/app/app-components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import {MoviesService} from "../../modules/movies/movie-services";
-import {IMovie} from "../../interfaces";
+import {IMovie, ISerial} from "../../interfaces";
 import { IMG_URL } from '../../constants';
+import {SerialsService} from "../../modules/serials/serials-services";
 
 
 @Component({
@@ -13,15 +14,21 @@ import { IMG_URL } from '../../constants';
 
 export class HomeComponent implements OnInit {
   movies: IMovie[];
+  serials: ISerial[];
   page:number = 1;
   IMG_URL: string = IMG_URL;
 
-  constructor(private moviesService: MoviesService) { }
+  constructor(private moviesService: MoviesService,
+              private serialsService: SerialsService) { }
 
   ngOnInit(): void {
     //@ts-ignore
     this.moviesService.getAll(this.page).subscribe(({results}) => {
       this.movies = results;
+    })
+    //@ts-ignore
+    this.serialsService.getAll(this.page).subscribe(({results}) => {
+      this.serials = results;
     })
   }
 
