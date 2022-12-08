ead541aaeb4210221a963bf24b6a0a5963f3a946
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-08 10:32:28 +0200

    home component

--- src/app/app-components/home/home.component.ts
+++ src/app/app-components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MoviesService} from "../../modules/movies/movie-services";
+import {IMovie} from "../../interfaces";
+import { IMG_URL } from '../../constants';
+
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
+  movies: IMovie[];
+  page:number = 1;
+  IMG_URL: string = IMG_URL;
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) { }
 
   ngOnInit(): void {
+    //@ts-ignore
+    this.moviesService.getAll(this.page).subscribe(({results}) => {
+      this.movies = results;
+    })
   }
 
 }
