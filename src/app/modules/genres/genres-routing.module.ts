6bac6756b54d7a812db096b74fc577e02834ca8e
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-12-11 23:15:55 +0200

    details genre movie

--- src/app/modules/genres/genres-routing.module.ts
+++ src/app/modules/genres/genres-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {GenresComponent} from "./genres-components/genres/genres.component";
 import {GenresMoviesListComponent} from "./genres-components/genres-movies-list/genres-movies-list.component";
+import {MovieDetailsComponent} from "../movies/movies-component/movie-details/movie-details.component";
 
 
 let routes: Routes = [
@@ -10,6 +11,7 @@ let routes: Routes = [
     path: '', component: GenresComponent, children: [
       {path: '', redirectTo: '/genres/35', pathMatch: "full"},
       {path: ':id', component: GenresMoviesListComponent},
+      {path: ':id/:id', component:MovieDetailsComponent}
     ]
   },
 ]
