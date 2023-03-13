46ecb5f7faa871646c40b350a63245f59dd2d310
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-03-13 13:41:43 +0200

    change

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -1,7 +1,10 @@
 import {environment} from "../../environments/environment";
 import {MY_KEY_API} from "./key.api";
+
+
 //@ts-ignore
 const {API: baseURL} = environment;
+
 
 export const urls = {
   movies: (page: number) => `${baseURL}/discover/movie?sort_by=popularity.desc&api_key=${MY_KEY_API}&page=${page}`,
